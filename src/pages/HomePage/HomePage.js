1d632d8269da93ac93daa3754dfdb5b57d947522
Emanuel Batista <emanuel.silva@uniara.edu.br>
2025-08-10 16:42:06 -0300

    Implement code changes to enhance functionality and improve performance

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,15 +1,19 @@
 import styles from "./HomePage.module.css";
 import googleBadge from "../../assets/google-badge.webp";
 import aplleBadge from "../../assets/apple-badge.webp";
+import printApp from "../../assets/print-app.jpg";
 
 
 function HomePage() {
     return (
         <div className={styles.homepage}>
-            <div className={styles.downloads}>
-                <img src={googleBadge} alt="Download no Google Play Store" className={styles.badge}/>
-                <img src={aplleBadge} alt="Download no Apple Store" className={styles.badge}/>
+            <div className={styles.container}>
+                <h1 className={styles.title}>Fecloc: o aplicativo de localização da <span className={styles.fec}>Fec!</span></h1>
+                <div className={styles.downloads}>
+                    <img src={googleBadge} alt="Download no Google Play Store" className={styles.badgeplay}/>
+                </div>
             </div>
+            <img src={printApp} alt="Aplicativo Fecloc" className={styles.printApp}/>
         </div>
     );
 }
