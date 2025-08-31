1c6d74c39a398fa410491a81b13c0d2d8e9f9aa4
Emanuel Silva <ebsilva1@uniara.edu.br>
2025-08-31 14:10:51 -0300

    feat: update dependencies and add new assets
    - Added @emotion/react, @emotion/styled, @mui/icons-material, and @mui/material to package.json.
    - Introduced a new SVG asset for the right arrow icon.
    - Refactored Navbar component to remove unused Menu and state management.
    - Simplified Navbar styles and adjusted layout for better responsiveness.
    - Updated HomePage component to include a background video and new CTA with an icon.
    - Cleaned up HomePage styles for improved layout and visual hierarchy.
    - Created a new index.js file in the Home directory of the web app.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,25 +1,17 @@
 import { Link } from 'react-router-dom';
-import { Menu, Logo } from "assets";
+import { Logo } from "assets";
 import styles from "./Navbar.module.css";
-import { useState } from "react";
+
 
 function Navbar() {
-    const [menuAberto, setMenuAberto] = useState(false);
 
-    return (
-        // Use className e o objeto 'styles' para aplicar a classe
-         <div className={styles.navbar}>
-      <Link to="/"><Logo /></Link>
 
-      <ul className={menuAberto ? `${styles.itensNav} ${styles.active}` : styles.itensNav}>
-        <li className={styles.itemNav}><Link to="/sobre">Sobre</Link></li>
-        <li className={styles.itemNav}><Link to="/contato">Contato</Link></li>             
-      </ul>
-
-      <Link to="/" className={styles.ctaButton}>Baixe agora!</Link> 
-      <Menu onClick={() => setMenuAberto(!menuAberto)} className={styles.menu}/>
+  return (
+    // Use className e o objeto 'styles' para aplicar a classe
+    <div className={styles.navbar}>
+      <Link to="/"><Logo className={styles.Logo} /></Link>
     </div>
-    );
+  );
 }
 
 export default Navbar;
