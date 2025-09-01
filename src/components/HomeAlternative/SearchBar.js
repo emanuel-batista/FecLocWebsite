fccb7f175e1686e96c20566d275b2696cf836291
Emanuel Batista <emanuel.silva@uniara.edu.br>
2025-09-01 20:29:06 -0300

    fix: add AccountCircleIcon to SearchBar component for user account access

--- src/components/HomeAlternative/SearchBar.js
+++ src/components/HomeAlternative/SearchBar.js
@@ -1,11 +1,14 @@
 import styles from './homealternative.module.css'
 import locIcon from '../../assets/icons/loc.png'; // <-- Import the image
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function SearchBar() {
     return (
         <div className={styles.SearchBar}>
             <img src={locIcon} alt="Ícone de pesquisa" className={styles.icon}/>
             <input type="text" placeholder="Pesquise por um curso para encontrar o stand!" className={styles.input}/>
+            <AccountCircleIcon className={styles.accountIcon} sx={{ fontSize: 16 }}/>
+
         </div>
     );
 }
