b3571083b683cbaea5b97ed1a3b862cf29b76cb6
Emanuel Batista <emanuel.silva@uniara.edu.br>
2025-09-01 16:33:05 -0300

    fix: update background image reference in HomeAlternative component

--- src/webApp/HomeAlternative/index.js
+++ src/webApp/HomeAlternative/index.js
@@ -1,12 +1,12 @@
 import PlaceCard from "components/HomeAlternative/PlaceCard";
 import styles from "./hAlternative.module.css";
+import cardniteImg from "./cardnite.jpg"; // <-- Import the image
 
 function HomeAlternative(){
-
     return(
         <div className={styles.hAlternative}>
             <h2>Unidades</h2>
-            <PlaceCard backgroundImage={/webApp/HomeAlternative/cardnite.jpg} standName={'Nite'} />
+            <PlaceCard backgroundImage={cardniteImg} standName={'Nite'} />
         </div>
     );
 }
